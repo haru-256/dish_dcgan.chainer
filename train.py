4460dee4e2767aee732520b8e83b53204f36d578
Yohei Okabayashi <yohei.kuro48@gmail.com>
2018-05-28 09:58:41 +0900

    conducted first experiments

---
+++ train.py
@@ -0,0 +1,127 @@
+import chainer
+from chainer import training
+from chainer.training import extensions
+from chainer.datasets import ImageDataset
+from chainer.serializers import save_npz
+
+# from discriminator import Discriminator  # Dence nobias
+from discriminator import Discriminator  # GAP nobias
+from generator import Generator
+from updater import DCGANUpdater
+from visualize import out_generated_image
+# from accuracy_reporter import accuracy_report
+import pathlib
+
+
+def make_optimizer(model, alpha=0.0002, beta1=0.5):
+    """
+    Setup an optimizer
+    """
+    optimizer = chainer.optimizers.Adam(alpha=alpha, beta1=beta1)
+    optimizer.setup(model)
+    # optimizer.add_hook(chainer.optimizer_hooks.WeightDecay(0.0001), 'hook_dec')
+
+    return optimizer
+
+
+def main():
+    import numpy as np
+    # fix seed
+    seed = 1
+    np.random.seed(seed)
+    if chainer.backends.cuda.available:
+        chainer.backends.cuda.cupy.random.seed(seed)
+
+    gpu = 0  # GAP: 0, Dense: 1
+    batch_size = 128
+    n_hidden = 100
+    epoch = 300  # Dence:100 GAP:300
+    out = "result_1_{}".format(seed)  # GAP:a, Dense:b
+
+    print('GPU: {}'.format(gpu))
+    print('# Minibatch-size: {}'.format(batch_size))
+    print('# n_hidden: {}'.format(n_hidden))
+    print('# epoch: {}'.format(epoch))
+    print('# out: {}'.format(out))
+    print('# seed: {}'.format(seed))
+    print('')
+
+    # Set up a neural network to train
+    gen = Generator()
+    dis = Discriminator()
+
+    if gpu >= 0:
+        # Make a specified GPU current
+        chainer.backends.cuda.get_device_from_id(gpu).use()
+        gen.to_gpu()  # Copy the model to the GPU
+        dis.to_gpu()
+
+    opt_gen = make_optimizer(gen)
+    opt_dis = make_optimizer(dis)
+
+    # Prepare Dataset
+    """
+    train = FaceData()
+    """
+    data_dir = pathlib.Path("./rsize_data_128")
+    abs_data_dir = data_dir.resolve()
+    print("data dir path:", abs_data_dir)
+    data_path = [path for path in abs_data_dir.glob("*.jpg")]
+    print("data length:", len(data_path))
+    data = ImageDataset(paths=data_path)  # dtype=np.float32
+    train_iter = chainer.iterators.SerialIterator(data, batch_size)
+
+    # Set up a updater and trainer
+    updater = DCGANUpdater(
+        models=(gen, dis),
+        iterator=train_iter,
+        optimizer={
+            'gen': opt_gen,
+            'dis': opt_dis
+        },
+        device=gpu)
+    trainer = training.Trainer(updater, (epoch, 'epoch'), out=out)
+
+    snapshot_interval = (10, 'epoch')
+    display_interval = (1, 'epoch')
+    # storage method is hdf5
+    trainer.extend(
+        extensions.snapshot(
+            filename='snapshot_iter_{.updater.iteration}.npz',
+            savefun=save_npz),
+        trigger=snapshot_interval)
+    trainer.extend(
+        extensions.snapshot_object(
+            gen, 'gen_iter_{.updater.iteration}.npz', savefun=save_npz),
+        trigger=snapshot_interval)
+    trainer.extend(
+        extensions.snapshot_object(
+            dis, 'dis_iter_{.updater.iteration}.npz', savefun=save_npz),
+        trigger=snapshot_interval)
+    trainer.extend(extensions.LogReport())
+    trainer.extend(
+        extensions.PrintReport([
+            'epoch', 'iteration', 'gen/loss', 'dis/loss', 'elapsed_time',
+            'dis/accuracy'
+        ]),
+        trigger=display_interval)
+    trainer.extend(extensions.ProgressBar(update_interval=20))
+    trainer.extend(
+        out_generated_image(gen, dis, 5, 5, seed, out),
+        trigger=display_interval)
+    # extensionにaccuaracy を求めるのはおかしい?
+    # L.Classifierの様にaccuracyをreportする
+    """
+    trainer.extend(accuracy_report(gen, dis, data=data),
+                   trigger=display_interval)
+    """
+    trainer.extend(
+        extensions.PlotReport(
+            ['gen/loss', 'dis/loss'], x_key='epoch', file_name='loss.png'))
+
+    # Run the training
+    trainer.run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ visualize.py
@@ -0,0 +1,60 @@
+import pathlib
+import numpy as np
+import matplotlib.pyplot as plt
+import chainer
+import chainer.backends.cuda
+from chainer import Variable
+
+
+def combine_images(generated_images):
+    total = generated_images.shape[0]
+    cols = int(np.sqrt(total))
+    rows = int(np.ceil(float(total) / cols))
+    width, height = generated_images.shape[1:3]
+    combined_image = np.zeros(
+        (height * rows, width * cols, 3), dtype=generated_images.dtype)
+
+    for index, image in enumerate(generated_images):
+        i = int(index / cols)
+        j = index % cols
+        for ch in range(3):
+            combined_image[width*i:width*(i+1), height*j:height*(j+1), ch] =\
+                image[:, :, ch]
+    return combined_image
+
+
+def out_generated_image(gen, dis, rows, cols, seed, dst):
+    @chainer.training.make_extension()
+    def make_image(trainer):
+        n_images = rows * cols
+
+        xp = gen.xp  # get module
+        xp.random.seed(seed)  # fix seed
+        z = Variable(gen.make_hidden(n_images, xp))
+        # test, evaluationの時は以下の２つを設定しなければならない
+        # https://qiita.com/mitmul/items/1e35fba085eb07a92560
+        # 'train'をFalseにすることで，train時とテスト時で挙動が異なるlayer(BN, Dropout)
+        # を制御する
+        with chainer.using_config('train', False):
+            # 'enable_backprop'をFalseとすることで，無駄な計算グラフの構築を行わない
+            # ようにしメモリの消費量を抑える.
+            with chainer.using_config('enable_backprop', False):
+                x = gen(z)
+        x = chainer.backends.cuda.to_cpu(x.data)
+        xp.random.seed()
+        np.random.seed()
+
+        x = (x * 127.5 + 127.5) / 255  # 0~255に戻し0~1へ変形
+        x = x.transpose(0, 2, 3, 1)  # NCHW->NHWCに変形
+        x = combine_images(x)
+        plt.imshow(x)
+        plt.axis("off")
+        preview_dir = pathlib.Path('{}/preview'.format(dst))
+        preview_path = preview_dir /\
+            'image_{:}epoch.png'.format(trainer.updater.epoch)
+        if not preview_dir.exists():
+            preview_dir.mkdir()
+        plt.tight_layout()
+        plt.savefig(preview_path)
+
+    return make_image
